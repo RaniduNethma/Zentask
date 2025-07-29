eaa483b208c8ed3d6afefb0f55ef202975164e6b
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-07-29 21:16:45 +0530

    Make Database Connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('')
+    await mongoose.connect('mongodb+srv://nethmaranidu:50zPY}$C[Hv;igz7GcA3c7y^)Z8;u!YfUDu9Q}0sg7Z5Qv6ekT@cluster0.w4iqbjb.mongodb.net/Zentask')
+        .then (() => console.log('Database Connected'));
 }
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { connectDB } from './config/db.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 //Middleware
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Database Connection
+connectDB();
+
+//Routes
+app.get('/', (req, res) => {
+    res.send('Api Working');
+})
+
+app.listen(port, () => {
+    console.log('Server Started on http://localhost:${port}');
+})
