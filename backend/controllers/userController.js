c550f8cef0ea5ad51be58e2dbc8e95028546f017
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-07-29 23:39:59 +0530

    create userModel & userController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,58 @@
+import User from './model/userModel.js';
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+//Register function
+export async function registerUser(req, res){
+    const{name, email, password} = req.body;
+
+    if(!name || !email || !password){
+        return res.status(400).json({
+            success: false,
+            message: "All fields are required"
+        })
+    }
+    if(!validator.isEmail(email)){
+        return res.status(400).json({
+            success: false,
+            message: "Invalid email address"
+        })
+    }
+    if(password.length < 8){
+        return res.status(400).json({
+            success: false,
+            message: "Password must be atleast 8 characters"
+        })
+    }
+
+    try{
+        if(await user.findOne({email})){
+            return res.status(409).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await User.create({name, email, password: hashed});
+        const token = createToken(user._id);
+
+        res.status(201).json({
+            success: true,
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Server error"
+        })
+    }
+}
