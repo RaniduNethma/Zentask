11dab57c24eb2daad36eb5f01049b69b210dfd64
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-07-30 23:50:04 +0530

    User Login Function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,19 +15,19 @@ export async function registerUser(req, res){
         return res.status(400).json({
             success: false,
             message: "All fields are required"
-        })
+        });
     }
     if(!validator.isEmail(email)){
         return res.status(400).json({
             success: false,
             message: "Invalid email address"
-        })
+        });
     }
     if(password.length < 8){
         return res.status(400).json({
             success: false,
             message: "Password must be atleast 8 characters"
-        })
+        });
     }
 
     try{
@@ -35,7 +35,7 @@ export async function registerUser(req, res){
             return res.status(409).json({
                 success: false,
                 message: "User already exists"
-            })
+            });
         }
 
         const hashed = await bcrypt.hash(password, 10);
@@ -50,15 +50,61 @@ export async function registerUser(req, res){
                 name: user.name,
                 email: user.email
             }
-        })
+        });
     }
     catch(error){
         console.log(error);
         res.status(500).json({
             success: false,
-            message: "Server error"
-        })
+            message: "Server Error"
+        });
     }
 }
 
 //User Login Function
+export async function userLogin(req, res){
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({
+            success: false,
+            message: "Email and Password required"
+        });
+    }
+
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        }
+        
+        const match = await bcrypt.compare(password, user.password);
+        if(!password){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        }
+
+        const token = createToken(user._id);
+        res.json({
+            success: true,
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+    }
+}
