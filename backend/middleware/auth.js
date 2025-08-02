87ca2fc4822191bad16571baad13bad65a85edfe
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-08-02 19:13:13 +0530

    Verify and attach user object

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -14,4 +14,26 @@ export default async function authMiddleware(req, res, next){
         });
     }
     const token = authHeader.split(' ')[1];
+
+    //Verify and attach user object
+    try {
+        const payload = jwt.verify(token, JWT_SECRET);
+        const user = await User.findById(payload.id).select('-password');
+        
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+        req.user = user;
+        next();
+    }
+    catch (error) {
+        console.log(error);
+        res.status(401).json({
+            success: false,
+            message: "Token invalid or expired"
+        });
+    }
 }
