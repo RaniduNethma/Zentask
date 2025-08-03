1ea654a4aaa6eac0865d9d2ffc51db3f009b03ee
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-08-04 00:31:34 +0530

    Secret scanning alert fixing

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://nethmaranidu:50zPY%7D%24C%5BHv%3Bigz7GcA3c7y%5E%29Z8%3Bu%21YfUDu9Q%7D0sg7Z5Qv6ekT@cluster0.w4iqbjb.mongodb.net/Zentask')
+    await mongoose.connect('')
         .then (() => console.log('Database Connected'));
 }
