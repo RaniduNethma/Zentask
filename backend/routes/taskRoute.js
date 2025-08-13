5ddaaa682149faafaf4f879cf5f58028977dff60
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-08-13 22:36:36 +0530

    create taskRouter

---
+++ backend/routes/taskRoute.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { createTask, getTasks, getTaskById, updateTask, deleteTask } from '../controllers/taskController.js'
+import authMiddleware from '../middleware/auth.js';
+
+const taskRouter = express.Router();
+
+taskRouter.route('/zt')
+    get(authMiddleware, getTasks);
+    post(authMiddleware, createTask);
+
+taskRouter.route('/:id/zt')
+    get(authMiddleware, getTaskById);
+    put(authMiddleware, updateTask);
+    delete(authMiddleware, deleteTask);
+
+export default taskRouter;
