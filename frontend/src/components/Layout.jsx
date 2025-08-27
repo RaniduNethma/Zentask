e3b7216f9852f4696699b7eaf356278ac4a4458b
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-08-27 22:57:33 +0530

    fetchTasks

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import NavBar from './NavBar';
 import { Sidebar } from 'lucide-react';
 import { Outlet } from 'react-router-dom';
@@ -15,17 +15,28 @@ const Layout = ({ onLogout, user }) => {
     setError(null);
 
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem('token')
       if(!token) throw new Error("No auth token found")
 
       const {data} = await axios.get("http://localhost:6500/api/task/zt", {
         headers: { Authorization: `bearer ${token}`}
       });
+
+      const arr = Array.isArray(data) ? data :
+        Array.isArray(data?.tasks) ? data.tasks :
+          Array.isArray(data?.tasks) ? data.data : []
+      setTasks(arr)
     }
-    catch (error) {
-      
+    catch (err) {
+      console.error(err); 
+      setError(err.message || "Could not load task")
+      if(err.response?.status === 401) onLogout()
+    } finally {
+      setLoading(false)  
     }
-  });
+  }, [onLogout]);
+  
+  useEffect(() => { fetchTasks() }, [fetchTasks]);
 
   return (
     <div className='min-h-screen bg-gray-50'>
@@ -35,7 +46,7 @@ const Layout = ({ onLogout, user }) => {
       <div className='ml-0 xl:ml-64 lg:ml-64 md:ml-64 pt-16 p-3 sm:p-4 md:p-4 transition-all duration-300 '>
         <div className='grid grid-cols-1 xl:grid-cols-3 gap-4 sm:gap-6'>
           <div className='xl:col-span-2 space-y-3 sm:space-y-4'>
-            <Outlet context={{ task, refreshTasks: fatchTasks }} />
+            <Outlet context={{ task, refreshTasks: fetchTasks }} />
           </div>
         </div>
       </div>
