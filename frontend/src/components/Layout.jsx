b0e6e07c4025e848a014198f9b6f5ccf05138fef
RaniduNethma <113999336+RaniduNethma@users.noreply.github.com>
2025-08-30 20:24:48 +0530

    Loading

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 const Layout = ({ onLogout, user }) => {
 
-  const [task, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -55,6 +55,7 @@ const Layout = ({ onLogout, user }) => {
     }
   }, [tasks]);
 
+  //Statistics Card
   const StatCard = ({ title, value, icon}) => (
     <div className='p-2 sm:p-3 rounded-xl bg-white shadow-sm border border-sky-100 hover:shadow-md transition-all 
     duration-300 hover:border-sky-100 group'>
@@ -74,15 +75,24 @@ const Layout = ({ onLogout, user }) => {
     </div>
   );
 
+  //Loading
+  if(loading) return (
+    <div className='min-h-screen bg-gray-50 flex items-center justify-center'>
+      <div className='animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-sky-500' />
+    </div>
+  )
+
+  
+
   return (
     <div className='min-h-screen bg-gray-50'>
       <NavBar user={user} onLogout={onLogout} />
-      <Sidebar user={user} task={task} />
+      <Sidebar user={user} tasks={tasks} />
 
       <div className='ml-0 xl:ml-64 lg:ml-64 md:ml-64 pt-16 p-3 sm:p-4 md:p-4 transition-all duration-300 '>
         <div className='grid grid-cols-1 xl:grid-cols-3 gap-4 sm:gap-6'>
           <div className='xl:col-span-2 space-y-3 sm:space-y-4'>
-            <Outlet context={{ task, refreshTasks: fetchTasks }} />
+            <Outlet context={{ tasks, refreshTasks: fetchTasks }} />
           </div>
         </div>
       </div>
